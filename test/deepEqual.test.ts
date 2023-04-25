6c21f0ecc5e448797c999d467f1b8cde87dfade3
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-04-25 19:36:02 +0800

    chore: deepEqual

---
+++ test/deepEqual.test.ts
@@ -0,0 +1,24 @@
+import { expect, it } from 'vitest'
+
+export default function deepEqual(x: any, y: any): boolean {
+  // @ts-expect-error
+  return x && y && typeof x === 'object' && typeof y === 'object'
+    ? Object.keys(x).length === Object.keys(y).length
+        // @ts-expect-error
+        && Object.keys(x).reduce((isEqual, key) => isEqual && deepEqual(x[key], y[key]), true)
+    : x === y
+}
+
+it('deepEqual', () => {
+  const a = 1
+  const b = 1
+  const obj = {
+    cc: 'c',
+    bb: {
+      kl: 2,
+    },
+  }
+  const obj2 = obj
+  expect(deepEqual(a, b)).toBe(true)
+  expect(deepEqual(obj, obj2)).toBe(true)
+})
