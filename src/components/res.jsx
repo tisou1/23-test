166cb7ae70f97dcf54dc08ba56b2b8e9c24e01d8
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-06-02 14:22:33 +0800

    chore: eeslint lint

--- src/components/res.jsx
+++ src/components/res.jsx
@@ -1,53 +1,47 @@
-/**
- * @file modules/job/components/ResponseResizer
- * @description 响应式容器
- * @author wangtao
- */
-
-import {useState, useRef} from 'react';
+import { useRef, useState } from 'react'
 
 function ResponseResizer(props) {
-    const {leftCard = null, rightCard = null} = props;
+  const { leftCard = null, rightCard = null } = props
 
-    const [siderWidth, setSiderWidth] = useState(594);
-    const [dragging, setDragging] = useState(false);
-    const [startPageX, setStartPageX] = useState(0);
+  const [siderWidth, setSiderWidth] = useState(594)
+  const [dragging, setDragging] = useState(false)
+  const [startPageX, setStartPageX] = useState(0)
 
-    const ref = useRef(null);
+  const ref = useRef(null)
 
-    const handleMouseDown = event => {
-        setStartPageX(event.pageX);
-        setDragging(true);
-    };
-    // 实时更新位置
-    const handleMouseMove = event => {
-        const curParentWidth = ref?.current.offsetWidth;
-        const currentSiderWidth = siderWidth + event.pageX - startPageX;
-        if (currentSiderWidth < 594 || curParentWidth - currentSiderWidth < 594) {
-            setDragging(false);
-            return;
-        }
-        setSiderWidth(currentSiderWidth);
-        setStartPageX(event.pageX);
-    };
-    const handleMouseUp = () => {
-        setDragging(false);
-        localStorage.setItem('siderWidth', siderWidth);
-    };
+  const handleMouseDown = (event) => {
+    setStartPageX(event.pageX)
+    setDragging(true)
+  }
+  // 实时更新位置
+  const handleMouseMove = (event) => {
+    const curParentWidth = ref?.current.offsetWidth
+    const currentSiderWidth = siderWidth + event.pageX - startPageX
+    if (currentSiderWidth < 594 || curParentWidth - currentSiderWidth < 594) {
+      setDragging(false)
+      return
+    }
+    setSiderWidth(currentSiderWidth)
+    setStartPageX(event.pageX)
+  }
+  const handleMouseUp = () => {
+    setDragging(false)
+    localStorage.setItem('siderWidth', siderWidth)
+  }
 
-    const handleMouseLeave = () => {
-        setDragging(false);
-    };
+  const handleMouseLeave = () => {
+    setDragging(false)
+  }
 
-    return (
+  return (
         <div className="response-main">
             <div className="workbench-root" ref={ref}>
                 <div
                     className="workbench-card"
-                    style={{userSelect: dragging ? 'none' : 'inherit'}}
+                    style={{ userSelect: dragging ? 'none' : 'inherit' }}
                     onMouseLeave={handleMouseLeave}
                 >
-                    <div className="workbench-card-left card-item" style={{width: siderWidth}}>
+                    <div className="workbench-card-left card-item" style={{ width: siderWidth }}>
                         {leftCard}
                     </div>
                     <div className="mosaic-split row" onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
@@ -57,17 +51,17 @@ function ResponseResizer(props) {
                     <div
                         onMouseMove={handleMouseMove}
                         style={{
-                            position: 'absolute',
-                            inset: '0px',
-                            zIndex: 90,
-                            backgroundColor: 'transpratent',
-                            display: dragging ? 'block' : 'none'
+                          position: 'absolute',
+                          inset: '0px',
+                          zIndex: 90,
+                          backgroundColor: 'transpratent',
+                          display: dragging ? 'block' : 'none',
                         }}
                     />
                 </div>
             </div>
         </div>
-    );
+  )
 }
 
-export default ResponseResizer;
+export default ResponseResizer
