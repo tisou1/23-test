49349882ad6e1e46518c749c2f7c3e1eec278c2b
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-09-06 15:20:22 +0800

    feat: add Marquee component

---
+++ src/components/Marquee.tsx
@@ -0,0 +1,41 @@
+
+import './Marquee.scss'
+
+type User = {
+  name: string
+  image: string
+}
+
+interface MarqueeProps {
+  users: User[]
+  reverse: boolean
+  dark: boolean
+}
+
+export default function Marquee(props: MarqueeProps) {
+  const {
+    users,
+    reverse
+  } = props
+  const colorScheme = props.dark ? 'light' : 'dark';
+
+  return (
+    <div>
+       <div className="marquee-wrapper overflow-hidden flex">
+            {Array(3)
+                .fill(users)
+                .map((users, index) => (
+                    <div key={index} className={`marquee${reverse ? ' marquee-reverse' : ''}`}>
+                        {users.map((user) => (
+                            <div className="w-full" key={user.name}>
+                                <img src={user.image} alt={`${user.name}-${colorScheme}`} />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+        </div>
+    </div>
+  )
+}
+
+
---
+++ src/pages/marquee/index.tsx
@@ -0,0 +1,24 @@
+import Marquee from '~/components/Marquee'
+
+export default function App() {
+  const usersData = ['fox', 'airbus', 'mercedes', 'ebay', 'ford', 'vw', 'intel', 'unicredit', 'lufthansa', 'nvidia', 'verizon', 'amex'];
+
+
+  const getUsersImages = () =>
+  usersData.map((name) => ({
+      name,
+      image: `https://primefaces.org/cdn/primereact/images/landing-new/whouses/${name}-dark.svg`
+  }));
+
+
+  const usersImages = getUsersImages();
+  const users1 = usersImages.slice(0, 6);
+  const users2 = usersImages.slice(6);
+
+
+  return (
+    <div>
+      <Marquee users={users1} />
+    </div>
+  )
+}
