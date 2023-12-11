8b38f33500044364bb5b9b3cb4f8c210eec808ea
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-12-11 21:35:02 +0800

    chore: 整理文件

--- test/add.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, it } from 'vitest'
-import add from './adder'
-
-it('add', () => {
-  expect(add(1, 2, 3)).toBe(6)
-  expect(add(1)(2)(3)).toBe(6)
-  expect(add(1, 2)(3)).toBe(6)
-})
