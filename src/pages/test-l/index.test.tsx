701a7711f5b1902c1a0ad194d903688f7c2b662c
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-12-29 18:15:38 +0800

    chore: add user-event library & add test case

--- src/pages/test-l/index.test.tsx
+++ src/pages/test-l/index.test.tsx
@@ -1,10 +1,21 @@
 import { render,screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import React  from 'react'
 import TestApp1 from './index'
 
 describe("test component", () => {
-  it('cc', () => {
+  it('基本渲染', () => {
     render(<TestApp1 />)
     expect(screen.getByText(/test-l/)).toBeInTheDocument();
+  })
+
+
+  it('用户事件',async () => {
+    render(<TestApp1 classname="test-input"/>)
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+    await userEvent.type(input, '1377');
+    expect(input).toHaveValue('1377');
+
   })
 })
--- src/pages/test-l/index.tsx
+++ src/pages/test-l/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import { InputText } from 'primereact/inputtext' 
 
-export default function TestApp1() {
+export default function TestApp1(props:any) {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
+    setInputValue(e.target.value);
+  }
 
   return (
-    <div className="test-l">test-l</div>
+    <div className="test-l">
+      <p>test-l</p>
+      <InputText value={inputValue} onChange={handleChange} className={props.calssname}/>
+    </div>
   )
 }
